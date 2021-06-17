e39c4310b3bf7995fef656e2fb17d61a58a4a873
Biprash <biprashgautam@gmail.com>
2021-06-17 15:06:16 +0545

    add response resource for models

---
+++ app/Http/Resources/NoteResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NoteResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->when($this->description, $this->desctiption),
+            'cover' => $this->when($this->cover, asset('storage', $this->cover)),
+        ];
+    }
+}
