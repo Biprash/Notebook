fc26a007fb51b8aaff7a05a5a16a65a715b11ab7
Biprash <biprashgautam@gmail.com>
2021-06-20 12:06:43 +0545

    register seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            NoteSeeder::class,
+            PageSeeder::class,
+            SectionSeeder::class,
+            ResourceSeeder::class,
+        ]);
     }
 }
