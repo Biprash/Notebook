8f14acf3ba64a25f58cf7d231e7478f17a1b5f40
Biprash <biprashgautam@gmail.com>
2021-06-28 10:12:22 +0545

    add recent controller

---
+++ app/Http/Controllers/Api/RecentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+class RecentController extends Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->session()->exists('notes.recently_viewed')){
+            $notes = $request->session()->get('notes.recently_viewed');
+            $notes = array_unique($notes);
+            $data = Note::whereIn('id', $notes)->get();
+            return $this->success('Recently Viewed Notes.', $data);
+        }
+        return $this->success('No Recent Notes found.', null, 204);
+    }
+}
