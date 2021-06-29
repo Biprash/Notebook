20b4a693bfeda8fb5dcaaabd9d4d9744fbeffd81
Biprash <biprashgautam@gmail.com>
2021-06-29 08:16:14 +0545

    add note policy

---
+++ app/Policies/NotePolicy.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class NotePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Note  $note
+     * @return mixed
+     */
+    public function view(User $user, Note $note)
+    {
+        return ($user->getKey() === $note->user->getKey()) || ($note->published_at !== null);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Note  $note
+     * @return mixed
+     */
+    public function update(User $user, Note $note)
+    {
+        return $user->getKey() === $note->user->getKey();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Note  $note
+     * @return mixed
+     */
+    public function delete(User $user, Note $note)
+    {
+        return $user->getKey() === $note->user->getKey();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Note  $note
+     * @return mixed
+     */
+    public function restore(User $user, Note $note)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Note  $note
+     * @return mixed
+     */
+    public function forceDelete(User $user, Note $note)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can publish the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Note  $note
+     * @return mixed
+     */
+    public function publish(User $user, Note $note)
+    {
+        return $user->getKey() === $note->user->getKey();
+    }
+}
