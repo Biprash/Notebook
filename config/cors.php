a5245902e77303875dda1c302cd478c2276a66f2
Biprash <biprashgautam@gmail.com>
2021-07-31 17:09:48 +0545

    feat: add register in api path

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout'],
 
     'allowed_methods' => ['*'],
 
